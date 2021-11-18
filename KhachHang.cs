359c8143089185744b646546dc90c03748c5186d
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2021-11-18 21:49:39 +0700

    Create KhachHang.cs

---
+++ KhachHang.cs
@@ -0,0 +1,11 @@
+namespace Quản_Lý_Nhân_Sự
+{
+    class KhachHang
+    {
+        public string MaKH { get; set; }
+        public string TenKH { get; set; }
+        public string GioiTinh { get; set; }
+        public string LoaiKH { get; set; }
+        public string NgaySinh { get; set; }
+    }
+}
