7df8a486648844d19905c47e3c3757c0a64ebd7e
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2021-11-18 21:49:00 +0700

    Create NhanVien.cs

---
+++ NhanVien.cs
@@ -0,0 +1,11 @@
+namespace Quản_Lý_Nhân_Sự
+{
+    class NhanVien
+    {
+        public string MaNV { get ; set ; }
+        public string TenNV { get ; set ; }
+        public string GioiTinh { get ; set ; }
+        public string BangCap { get ; set ; }
+        public string Ngaysinh { get ; set ; }
+    }
+}
